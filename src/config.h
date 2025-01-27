5596a9d16aef3b96b80e341665f7137283bb8695
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-01-27 08:27:33 +0100

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,5 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
+#include <stdint.h>
 
 #define KERNEL_CODE_SELECTOR 0x08
 #define KERNEL_DATA_SELECTOR 0x10
@@ -11,5 +12,10 @@
 #define OS_HEAP_SIZE_BYTES  104857600 // 100 MB
 #define OS_HEAP_BLOCK_SIZE  4096
 #define OS_HEAP_ADDRESS     0x01000000
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef int8_t i8;
+typedef int16_t i16;
 
 #endif
