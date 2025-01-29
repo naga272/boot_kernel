7fb07dc1421e89f24e9597329162c7ab802698a7
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-01-29 06:47:02 +0100

    Add files via upload

---
+++ src/utilities/stdlib/stdlib.h
@@ -0,0 +1,18 @@
+
+#ifndef STDLIB_H
+    #define STDLIB_H
+
+#include <stddef.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#if defined(CODE_FOR_HEAP)
+    void* current_address = (void*) (0x1000000);
+    void* malloc(size_t);
+#endif
+
+
+void* memset(void* ptr, int c, size_t size);
+
+#endif
