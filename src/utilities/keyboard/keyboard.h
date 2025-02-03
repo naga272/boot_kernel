3f2d2aa6f6c36cde23f6936e63fea009aeb0252a
Nagato272 <114398392+naga272@users.noreply.github.com>
2025-02-03 04:45:13 +0200

    Delete src directory

--- src/utilities/keyboard/keyboard.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-#include <stdint.h>
-
-extern unsigned char keyboard_map_QWERTY[128];  // Dichiarazione della variabile globale
-extern unsigned char keyboard_map_QZERTY[128];  // Dichiarazione della variabile globale
-#endif
--- src/utilities/memory/kheap.h
+++
@@ -1,8 +0,0 @@
-#ifndef KHEAP_H
-    #define KHEAP_H
-
-
-// funzione che inizializza l'heap del kernel.
-void kheap_init();
-
-#endif
